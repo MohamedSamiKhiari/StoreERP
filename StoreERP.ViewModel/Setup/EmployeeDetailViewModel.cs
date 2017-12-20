4ef8b50b7185d46f5e67bd153cda118bb75fa005
Er. Binod Mehta <mehtabinod2012@gmail.com>
2017-12-20 23:40:36 +0545

    ViewModel

---
+++ StoreERP.ViewModel/Setup/EmployeeDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StoreERP.Model.Setup;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreERP.ViewModel.Setup
+{
+    public class EmployeeDetailViewModel
+    {
+        public EmployeeDetailModel dbModel { get; set; }
+    }
+}
